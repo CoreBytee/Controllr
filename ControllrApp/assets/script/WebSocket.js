609379c07f9f3a93d18fe1aaf4b64329a194f271
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-01 14:44:32 +0100

    create client side websocket class

---
+++ ControllrApp/assets/script/WebSocket.js
@@ -0,0 +1,63 @@
+class WebSocketClass {
+    constructor(ConnectionCode) {
+        this.Listeners = {}
+
+        this.RawWebsocket = new WebSocket(`wss://ccws.skystuff.cc/connect/Controllr${ConnectionCode}/Controllr`)
+        this.RawWebsocket.onopen = (Event) => { this.Emit("Connect", Event) }
+        this.RawWebsocket.onmessage = (Event) => { this.Emit("TextMessage", Event.data) }
+        this.RawWebsocket.onclose = (Event) => { this.Emit("Disconnect", Event) }
+
+        this.On(
+            "TextMessage",
+            (Data) => {
+                try {
+                    Data = JSON.parse(Data)
+                } catch (e) {
+                    return
+                }
+                this.Emit("Data", Data)
+            }
+        )
+
+        this.PingInterval = setInterval(
+            () => {
+                this.SendData(
+                    { Type: "Ping", SentAt: Date.now()}
+                )
+            },
+            1000
+        )
+    }
+
+    On(Event, Callback) {
+        if (!this.Listeners[Event]) {
+            this.Listeners[Event] = []
+        }
+        this.Listeners[Event].push(Callback)
+        return Callback
+    }
+
+    Emit(Event, Data) {
+        if (this.Listeners[Event] != undefined) {
+            this.Listeners[Event].forEach((Callback) => {
+                Callback(Data)
+            })
+        }
+    }
+
+    Close(Code) {
+        this.RawWebsocket.close(Code)
+    }
+
+    SendText(Text) {
+        this.RawWebsocket.send(Text)
+    }
+
+    SendData(Data) {
+        this.SendText(
+            JSON.stringify(Data)
+        )
+    }
+}
+
+window.WebSocketClass = WebSocketClass
