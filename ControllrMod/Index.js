6f48f8020e1a6442a258b7d306daec5ec08bef9b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-13 14:05:35 +0100

    add interactions

--- ControllrMod/Index.js
+++ ControllrMod/Index.js
@@ -1,5 +1,9 @@
 const RandomNumber = require("./Helpers/RandomNumber.js")
 const Event = require("./Helpers/Event.js")
+const FrameEvent = require("./Helpers/FrameEvent.js")
+Hud.clearDraw2Ds()
+Hud.clearDraw3Ds()
+
 function Clamp(Value, Min, Max) {
     return Math.min(Math.max(Value, Min), Max)
 }
@@ -11,11 +15,15 @@ ControllrMod.ConnectionCode = 111111 //RandomNumber(6)
 ControllrMod.Websocket = new (require("./Classes/Websocket.js"))(ControllrMod.ConnectionCode)
 ControllrMod.Websocket.Connect()
 ControllrMod.Websocket.SendData("Hello")
+ControllrMod.PlayerOrientation = {X: 0, Y: 0}
+ControllrMod.CrouchState = false
+ControllrMod.JumpState = false
+ControllrMod.SprintState = false
 
 ControllrMod.Websocket.On(
     "Data",
     (Data) => {
-        print(Data)
+        // print(Data)
         if (Data.Type == "Ping") {
             ControllrMod.Websocket.SendData(
                 {
@@ -25,15 +33,53 @@ ControllrMod.Websocket.On(
                 }
             )
             ControllrMod.LastPingTime = Date.now()
+        } else if (Data.Type == "Movement") {
+            ControllrMod.MovementState = Data
+        } else if (Data.Type == "View") {
+            ControllrMod.ViewState = Data
+        } else if (Data.Type == "Punch") {
+            print("Punch")
+        } else if (Data.Type == "Crouch") {
+            ControllrMod.CrouchState = Data.State
+        } else if (Data.Type == "Jump") {
+            ControllrMod.JumpState = Data.State
+        } else if (Data.Type == "Sprint") {
+            ControllrMod.SprintState = Data.State
+        } else if (Data.Type == "Interact") {
+            print("Interact")
         }
     }
 )
 
-Event(
-    "Tick",
+let LastFrameTime = Time.time()
+
+FrameEvent(
     () => {
-        if (ControllrMod.LastPingTime + 10000 < Date.now()) {
+
+        const Now = Time.time()
+        const DeltaTime = (Now - LastFrameTime) / 1000
+        LastFrameTime = Now
+
+        if (ControllrMod.LastPingTime + 5000 < Date.now()) {
             Chat.actionbar("Controllr is running with code " + ControllrMod.ConnectionCode)
         }
+
+        let MovementState = ControllrMod.MovementState
+        if (!MovementState) {
+            MovementState = {X: 0, Y: 0}
+        }
+
+        let ViewState = ControllrMod.ViewState
+        if (!ViewState) {
+            ViewState = {X: 0, Y: 0}
+        }
+
+        ControllrMod.PlayerOrientation.X += ViewState.X * 150 * DeltaTime
+        ControllrMod.PlayerOrientation.Y += ViewState.Y * 150 * DeltaTime
+        ControllrMod.PlayerOrientation.Y = Clamp(ControllrMod.PlayerOrientation.Y, -90, 90)
+
+        const Input = Player.createPlayerInput(-MovementState.Y, -MovementState.X, ControllrMod.PlayerOrientation.X, ControllrMod.PlayerOrientation.Y, ControllrMod.JumpState, ControllrMod.CrouchState, ControllrMod.SprintState)
+        Player.clearInputs()
+        Player.addInput(Input)
     }
 )
